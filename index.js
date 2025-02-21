b2dd766158dd7fd1d74f426a81d313f27ad510f4
James Harrison <james@fasttortoise.co.uk>
2025-02-21 17:24:24 +0000

    📦 Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Libs
+import express from 'express'
+import http from 'http'
+const app = express()
+const port = 3000
+
+// Start the server
+const httpServer = http.createServer(app)
+httpServer.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+app.use('*', (_req, res) =>
+    res.send("woo")
+)
+
+// SocketIO
+import socket from './socket.js'
+export const io = socket(httpServer)
